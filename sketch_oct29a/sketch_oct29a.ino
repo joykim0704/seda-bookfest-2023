d07ca06d5e916f9f5ae8e4313d63fe4cf8807bc6
thezoot3 <thezoot3@gmail.com>
2023-11-01 20:40:21 +0900

    asdfghj

--- sketch_oct29a/sketch_oct29a.ino
+++ sketch_oct29a/sketch_oct29a.ino
@@ -1,38 +1,23 @@
-#include "SevenSeg.h";
-SevenSeg dispTen(2,3,4,5,6,7,8);
-SevenSeg dispOne(2,3,4,5,6,7,8);
-const int numofDigitsTen = 1;
-const int numofDigitsOne = 1;
-int digitPinsTen[1] = {A1};
-int digitPinsOne[1] = {A2};
+#include <Servo.h>;
+
+//int targetSensor[3] = []
+
+Servo servo;
+int servoPos = 0;
+int count = 0;
+
 void setup() {
-  dispTen.setDigitPins(numofDigitsTen, digitPinsTen);
-  dispTen.setDigitDelay(1667);
-  dispOne.setDigitPins(numofDigitsOne, digitPinsOne);
-  dispOne.setDigitDelay(1667);
-  
+  Serial.begin(9600);
+  servo.attach(9);
 }
-int digitWrite[2] = {0,0};
 void loop() {
-  dispTen.write(digitWrite[0]);
-  dispTen.clearDisp();
-  dispOne.write(digitWrite[1]);
-  dispOne.clearDisp();
-}
-void addCounter() {
-  if(digitWrite[1] == 9) {
-    if(digitWrite[0] == 9) {
-      digitWrite[0] = 0;
-      digitWrite[1] = 0;
-    }
-    digitWrite[0] = digitWrite[0] + 1;
-    digitWrite[1] = 0;
-  } else {
-    digitWrite[1] = digitWrite[1] + 1;
+  Serial.println(analogRead(A2));
+  if(analogRead(A2) > 1000) {
+    Serial.println(count++);
+
   }
 }
-void clearCounter() {
-  digitWrite[0] = 0;
-  digitWrite[1] = 0;
+void reloadTarget() {
+  servoPos = 180;
+  servo.write(180);
 }
-
