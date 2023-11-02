09b1f1d8e888695dee05f47affa5e06b2bc3c23b
thezoot3 <thezoot3@gmail.com>
2023-11-02 16:29:45 +0900

    asdfgh

--- sketch_oct29a/sketch_oct29a.ino
+++ sketch_oct29a/sketch_oct29a.ino
@@ -39,11 +39,18 @@ void segClear(){
 Servo servo;
 int servoPos = 0;
 int score = 0; //점수
-int check1,check2 = 0; //과녁 상태(쓰러짐=1, 쓰러지지 않음=0)
+bool check1,check2 = false; //과녁 상태(쓰러짐=1, 쓰러지지 않음=0)
+bool reloading = false;
+
 
 void setup() {
   Serial.begin(9600);
   servo.attach(9);
+  for(servoPos; servoPos > 0) {
+    servoPos-= 5
+    servo.write(servoPos);
+    delay(50)
+  }
   for(int i=0;i<9;i++){
     pinMode(segPin[i], OUTPUT); //digital 2~8번핀 세그먼트
   }
@@ -52,29 +59,39 @@ void setup() {
   digitalWrite(digitPin[0], LOW); //전류X
   digitalWrite(digitPin[1], LOW); //전류O
 }
+unsigned long lastExecutionTime = 0;  // 마지막 작업 실행 시간을 저장할 변수
+unsigned long interval = 50;        // 작업을 수행할 간격 (50ms)
+
 void loop() {
-  if(analogRead(A3) > 1000) { //과녁1 쓰러짐
-    score+=5; //점수+5
-    check1=1; //쓰러짐 체크
-    digitalWrite(10,HIGH); //LED1 켜기
+  unsigned long currentTime = millis();
+  if (reloading && currentTime - lastExecutionTime >= interval) {
+    if(servoPos < 180) {
+      servoPos+=5;
+      servo.write(servoPos);
+    } else {
+      digitalWrite(10, HIGH);
+      digitalWrite(11, HIGH);
+      reloading = false;
+    }
+    lastExecutionTime = currentTime; 
   }
-  if(analogRead(A4) > 1000) { //과녁2 쓰러짐
+  if(analogRead(A3) > 1000 && !check1) { //과녁1 쓰러짐
     score+=5; //점수+5
-    check2=1; //쓰러짐 체크
-    digitalWrite(11,HIGH); //LED2 켜기
+    check1=true; //쓰러짐 체크
+    digitalWrite(10,LOW); //LED1 끄기
+  }
+  if(analogRead(A4) > 1000 && !check2) { //과녁2 쓰러짐
+    score+=5; //점수+5
+    check2=true; //쓰러짐 체크
+    digitalWrite(11,LOW); //LED2 끄기
   }
   d1=score%10;//1의 자리 변수
   d2=(score-score%10)/10;//10의 자리 변수
   segOutput(1,d1,1); //1의 자리 세그먼트에 표시
   segOutput(0,d2,1); //10의 자리 세그먼트에 표시
-  if(check1==check2==1)
-  {
-    reloadTarget();//과녁 되돌리기
+  if(check1&&check2&&!reloading) {
+    reloading = true;
+    digitalWrite(10, LOW);
+    digitalWrite(11, LOW);
   }
 }
-void reloadTarget() {
-  servoPos = 180;
-  servo.write(180);//서보모터 돌리기
-  digitalWrite(10,LOW);//LED1 끄기
-  digitalWrite(11,LOW);//LED2 끄기
-  }
